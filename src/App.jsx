e2746f5ee36f769cc970425852d0b2e3490bd861
EvKykot <e.kykot@gmail.com>
2016-12-22 21:24:18 +0200

    phase 2 (Change the value passed to the function. Now, we do not pass a string, now we passed to function object.)

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ let state = 0;
 
 function updateState(state, action) {
 	if (action === 'INCREMENT') {
-		return state + 1;
+		return state + action.amount;
 	} else if (action === 'DECREMENT') {
-		return state -1;
+		return state - action.amount;
 	} else {
 		return state;
 	}
 }
 
-state = updateState(state, 'INCREMENT');
+const incrementAction = { type: 'INCREMENT', amount: 5 };
+const decrementAction = { type: 'DECREMENT', amount: 3 };
+
+state = updateState(state, incrementAction);
 console.log(state);
 
-state = updateState(state, 'DECREMENT');
+state = updateState(state, decrementAction);
 console.log(state);
 
-state = updateState(state, 'NOTHING');
+state = updateState(state, {});
 console.log(state);
