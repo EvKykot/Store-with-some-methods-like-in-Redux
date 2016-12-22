4d41706a87e0a82a95c2b3720fee5d8df1436219
EvKykot <e.kykot@gmail.com>
2016-12-22 21:33:07 +0200

    phase 3 (Creating a store like a class and add to store methods of update and get state.)

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,31 @@ function updateState(state, action) {
 	}
 }
 
+class Store {
+	constructor(updateState, state) {
+		this._updateState = updateState;
+		this._state = state;
+	}
+
+	get State() {
+		return this._state;
+	}
+
+	update(action) {
+		this._state = this._updateState(this._state, action);
+	}
+}
+
+const store = new Store(updateState, 0);
+
 const incrementAction = { type: 'INCREMENT', amount: 5 };
 const decrementAction = { type: 'DECREMENT', amount: 3 };
 
-state = updateState(state, incrementAction);
-console.log(state);
+store.update(incrementAction);
+console.log(store.state);
 
-state = updateState(state, decrementAction);
-console.log(state);
+store.update(decrementAction);
+console.log(store.state);
 
-state = updateState(state, {});
-console.log(state);
+store.update({});
+console.log(store.state);
